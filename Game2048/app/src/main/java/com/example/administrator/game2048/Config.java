eb15a5192c875abec6ba467c7fbbc7e5d691d82e
wangruole <729433219@qq.com>
2017-05-08 22:24:29 +0800

    2048游戏

---
+++ Game2048/app/src/main/java/com/example/administrator/game2048/Config.java
@@ -0,0 +1,10 @@
+package com.example.administrator.game2048;
+
+/**
+ * Created by Administrator on 2017/5/8 0008.
+ */
+
+public class Config {
+    public static final int LINES = 4;
+    public static int CARD_WIDTH = 0;
+}
